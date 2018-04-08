e387f2edcd48bd3c4936627de418697807630405
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-04-08 19:02:52 +0200

    Update accordion

---
+++ .storybook/stories/accordion/Accordion.js
@@ -0,0 +1,58 @@
+// @flow
+import * as React from 'react';
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionPanel,
+} from '../../../packages/accordion';
+
+type Props = {
+  renderHeader: (i: number) => string | React.Node,
+  className?: string,
+  allowTogle?: boolean,
+  allowMultiple?: boolean,
+  expandedIds?: Array<string>,
+};
+
+const getItems = (expandedIds, renderHeader) => {
+  let items = [];
+  for (let i = 0; i <= 2; i += 1) {
+    items = [
+      ...items,
+      <AccordionHeader
+        key={`header-${i}`}
+        id={`header-${i}`}
+        controls={`panel-${i}`}
+      >
+        {renderHeader(i)}
+      </AccordionHeader>,
+      <AccordionPanel
+        key={`panel-${i}`}
+        id={`panel-${i}`}
+        isExpanded={expandedIds && expandedIds.indexOf(i) >= 0}
+      >
+        <ul>
+          <li><input /></li>
+          <li><input /></li>
+        </ul>
+      </AccordionPanel>,
+    ];
+  }
+  return items;
+};
+
+const AccordionTest = ({ expandedIds, renderHeader, ...props }: Props) => (
+  <Accordion {...props}>
+    {getItems(expandedIds, renderHeader)}
+  </Accordion>
+);
+
+AccordionTest.defaultProps = {
+  className: null,
+  allowMultiple: false,
+  allowTogle: false,
+  expandedIds: null,
+  renderHeader: i => `Header ${i + 1}`,
+};
+
+export default AccordionTest;
--- .storybook/stories/accordion/index.js
+++ .storybook/stories/accordion/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { AccordionTest } from '../../../packages/accordion';
+import Accordion from './Accordion';
 
 storiesOf('Accordion', module)
-  .add('always single panel opened', () => <AccordionTest />)
-  .add('allow toggle panel', () => <AccordionTest allowToggle />)
-  .add('allow multi-selectable panel', () => <AccordionTest allowMultiple />);
+  .add('always single panel opened', () => <Accordion />)
+  .add('allow toggle panel', () => <Accordion allowToggle />)
+  .add('allow multi-selectable panel', () => <Accordion allowMultiple />);
