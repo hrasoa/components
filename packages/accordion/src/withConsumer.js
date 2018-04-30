4045fa27085b390200287a08d1575a1b282f5a92
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-04-30 18:38:42 +0200

    Update accordion

--- packages/accordion/src/withConsumer.js
+++ packages/accordion/src/withConsumer.js
@@ -3,11 +3,11 @@ import * as React from 'react';
 import getDisplayName from '@hrasoa/components-utils/getDisplayName';
 
 type ProviderValue = {
-  isDisabled: (id: string) => boolean,
-  isExpanded: (id: string) => boolean,
-  isFocused: (id: string) => boolean,
-  togglePanel: (id: string) => void,
-  addPanel: (id: string) => void,
+  isDisabled: (panlelId: string) => boolean,
+  isExpanded: (panlelId: string) => boolean,
+  isFocused: (panlelId: string) => boolean,
+  togglePanel: (panlelId: string) => void,
+  addPanel: (panlelId: string) => void,
   handleKeyNavigation: (e: SyntheticKeyboardEventElement<HTMLElement>) => void,
   openAll: () => void,
   closeAll: () => void,
